0b0ad964a651d8154cf53bd2af3b921e6504665a
Willie Wheeler <willie.wheeler@gmail.com>
2020-11-20 23:51:51 -0800

    Fixed transition matrix

--- markov_demo.py
+++ markov_demo.py
@@ -12,7 +12,7 @@ import random
 # Markov model
 states = [ 'Drinking', 'Sightseeing', 'Hiking', 'Kayaking' ]
 transitions = [
-    [ 0.4, 0.6, 0.0, 0.8 ],
+    [ 0.4, 0.6, 0.0, 0.0 ],
     [ 0.2, 0.0, 0.8, 0.0 ],
     [ 0.3, 0.0, 0.5, 0.2 ],
     [ 0.1, 0.0, 0.2, 0.7 ]
